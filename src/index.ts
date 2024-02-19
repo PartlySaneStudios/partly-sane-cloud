ba946d315c3edfd2a0867851efdab462a78ac269
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-02-19 10:46:40 -0800

    Clean index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { loadApi } from './api/api';
 
-import dotenv from 'dotenv'
-import { cleanCache } from './backend/CleanCache';
+import dotenv from 'dotenv';
 import { loadBackend } from './backend/backend';
 
-const CLEAN_CACHE_MINUTES = 1
 async function main() {
   dotenv.config()
 
