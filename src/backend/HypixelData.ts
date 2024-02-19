a9108fa00e31d272c8b34f092d7c2a327487e681
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-02-18 18:14:50 -0800

    Added dotenv

--- src/backend/HypixelData.ts
+++ src/backend/HypixelData.ts
@@ -2,16 +2,15 @@
 
 // https://api.hypixel.net/v2/skyblock/profiles
 
-import { env } from "node:process"
+import { env } from "process"
 
-async function requestSkyblockProfilesEndpoint(uuid: string): Promise<string> {
-
+export async function requestSkyblockProfilesEndpoint(uuid: string): Promise<string> {
   const url = `https://api.hypixel.net/v2/skyblock/profiles?key=${env.HYPIXEL_API_KEY}&uuid=${uuid}`
   try {
-    const response = await (await fetch(url)).json()
+    const response = await (await fetch(url)).text()
     return response
   } catch(exception) {
     console.error(exception)
+    return "error"
   }
-  return ""
 }
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+import { requestSkyblockProfilesEndpoint } from "./backend/HypixelData"
+import dotenv from 'dotenv'
+
+async function main() {
+  dotenv.config()
+
+  requestSkyblockProfilesEndpoint("d3edf183-d150-40a4-9754-5df5669c8878").then((response) => {
+    console.log(`Response:${response}`)
+  })
+}
+
+
+main()
